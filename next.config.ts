028ede7a8b502b4eaf668ece1d1f32696791b9ab
Franky Hung <arkon.digital.hcf@gmail.com>
2024-11-20 17:50:23 +0800

    added static image import

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    images: {
+        formats: ["image/webp"],
+        loader: "default",
+        path: "/_next/image",
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "zdmvmqnfxspqronkjrxn.supabase.co",
+                port: "",
+                pathname: "**",
+            },
+            {
+                protocol: "https",
+                hostname: "kuqudkdgwebajssmbczl.supabase.co",
+                port: "",
+                pathname: "**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
